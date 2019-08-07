31a7f41de909aac1bca287ce82981b9fa15a6560
Macbright007 <nbright239@gmail.com>
2019-08-07 16:49:19 +0100

    my first Flask commit

---
+++ myflaskapp/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique',
+            'author':'Mcbright',
+            'create_date':'11-10-2018'
+        },
+        {
+            'id': 2,
+            'title':'Article two',
+            'body':'This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique',
+            'author':'James',
+            'create_date':'10-10-2018'
+        },
+        {
+            'id': 3,
+            'title':'Article three',
+            'body':'This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique',
+            'author':'McDonald',
+            'create_date':'09-10-2018'
+        }
+    ]
+    return articles
